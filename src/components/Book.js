8432917b9cd1e6e3ac4efd666b5ee16f5d0abfbb
Surender Boora <surender.boora@druva.com>
2017-09-05 23:49:56 +0530

    refactor: Moved value on select tag
    Moved value to be selected from option to select tag rather than checking
    on each of the option to set selected.

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,27 +1,31 @@
 import React, { Component } from 'react';
 
 class Book extends Component {
-    render() {
-        const book = this.props.book;
-        return (
-            <div className="book">
-              <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                <div className="book-shelf-changer">
-                  <select>
-                    <option value="none" disabled>Move to...</option>
-                    <option selected = {book.shelf === 'currentlyReading'} value="currentlyReading">Currently Reading</option>
-                    <option selected = {book.shelf === 'wantToRead'} value="wantToRead">Want to Read</option>
-                    <option selected = {book.shelf === 'read'} value="read">Read</option>
-                    <option selected = {book.shelf === 'none'} value="none">None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-title">{book.title}</div>
-              <div className="book-authors">{book.authors.join(', ')}</div>
-            </div>
-        );
-    }
+  render() {
+    const book = this.props.book;
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover"
+            style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}>
+          </div>
+          <div className="book-shelf-changer">
+            <select
+              onChange={(e) => this.props.onShelfChange(e.target.value, book)}
+              value={book.shelf}>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors.join(', ')}</div>
+      </div>
+    );
+  }
 }
 
 export default Book;
